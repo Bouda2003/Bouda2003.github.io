844a36068b51c392db5ae655fffa99c214adef84
Bouda2003 <123108236+Bouda2003@users.noreply.github.com>
2025-04-24 22:30:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,9 +23,12 @@ function handleOrientation(event) {
     const alpha = event.alpha;
     if (alpha != null) {
         const compassCircle = document.querySelector('.compass-circle');
-        compassCircle.style.transform = `translate(-50%, -50%) rotate(${-alpha}deg)`;
+        requestAnimationFrame(() => {
+            compassCircle.style.transform = `translate(-50%, -50%) rotate(${-alpha}deg)`;
+        });
     }
 }
+
 
 
 // Wait for user to click anywhere to ask for permission
